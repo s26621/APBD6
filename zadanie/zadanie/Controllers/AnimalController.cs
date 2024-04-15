c2eade4e22e44ac6536af782ac3500a37b4cb77c
Loczo <truskawka643@gmail.com>
2024-04-15 13:26:00 +0200

    Wszystko poza Connection

--- zadanie/zadanie/Controllers/AnimalController.cs
+++ zadanie/zadanie/Controllers/AnimalController.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using zadanie.DTO;
+using zadanie.Models;
 using zadanie.Services;
 
 namespace zadanie.Controllers;
 
+[Route("api/[controller]")]
+[ApiController]
 public class AnimalController : ControllerBase
 {
     private readonly IAnimalService _animalService;
@@ -10,5 +14,32 @@ public class AnimalController : ControllerBase
     public AnimalController(IAnimalService animalService)
     {
         _animalService = animalService;
+    }
+
+    [HttpGet]
+    public IActionResult GetAnimals(AnimalOrderBy? orderBy)
+    {
+        var animals = _animalService.GetAnimals(orderBy);
+        return Ok(animals);
+    }
+    
+    [HttpPost]
+    public IActionResult CreateAnimal(AnimalDTO animal)
+    {
+        var affectedCount = _animalService.CreateAnimal(animal);
+        return StatusCode(StatusCodes.Status201Created);
+    }
+    
+    [HttpPut("{id:int}")]
+    public IActionResult UpdateAnimal(int id, AnimalDTO animal)
+    {
+        var affectedCount = _animalService.UpdateAnimal(animal);
+        return NoContent();
+    }
+    [HttpDelete("{id:int}")]
+    public IActionResult DeleteAninal(int id)
+    {
+        var affectedCount = _animalService.DeleteAnimal(id);
+        return NoContent();
     }
 }
--- zadanie/zadanie/Program.cs
+++ zadanie/zadanie/Program.cs
@@ -4,7 +4,6 @@ using zadanie.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
